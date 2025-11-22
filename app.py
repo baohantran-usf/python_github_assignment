4a0eaf43e3d1744bbea7eebb212fad1c0a4a3be7
baohantran-usf <baohantran@usf.edu>
2025-11-21 20:41:46 -0500

    Clean up code, add comments, finalize program

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+#A welcom message
 print("Welcome to my Python Program.")
 
+#Define variable
 saving = input("How many did you save this month? ")
 
-saving = float(saving)
-annually_saving = saving * 12
+#Type conversion and Calculation
+saving = float(saving) #Convert the variable
+annually_saving = saving * 12 #Calculate the amount of annual saving
 
-print(f"You are on track to save ${annually_saving} this year.")
+print(f"You are on track to save ${annually_saving} this year.") #Print the output 
 
+#Handling Error
 try:
     saving = float(saving)
 except ValueError:
