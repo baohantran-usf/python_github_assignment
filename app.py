930debac9c43c50a9afd0b4c6e730d92fffffe10
baohantran-usf <baohantran@usf.edu>
2025-11-21 20:37:05 -0500

    Add basic error handling for user input

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ saving = float(saving)
 annually_saving = saving * 12
 
 print(f"You are on track to save ${annually_saving} this year.")
+
+try:
+    saving = float(saving)
+except ValueError:
+    print("Please enter a valid number.")
+    exit()
